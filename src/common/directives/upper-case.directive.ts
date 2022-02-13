ddaada7cd6a0ff90202e84a18bd5122c752b5397
GreatLaboratory <cartopia95@naver.com>
2022-02-13 18:14:23 +0900

    refactor: directive arrow function

--- src/common/directives/upper-case.directive.ts
+++ src/common/directives/upper-case.directive.ts
@@ -1,14 +1,10 @@
 import { MapperKind, mapSchema } from '@graphql-tools/utils'
 import { defaultFieldResolver, GraphQLSchema } from 'graphql'
 
-export function upperDirectiveTransformer(schema: GraphQLSchema, directiveName: string): GraphQLSchema {
+export const upperDirectiveTransformer = (schema: GraphQLSchema, directiveName: string): GraphQLSchema => {
 	return mapSchema(schema, {
 		[MapperKind.OBJECT_FIELD]: (fieldConfig) => {
-			if (
-				fieldConfig.astNode &&
-				fieldConfig.astNode.directives &&
-				fieldConfig.astNode.directives[0].name.value === directiveName
-			) {
+			if (checkDirective(fieldConfig, directiveName)) {
 				const { resolve = defaultFieldResolver } = fieldConfig
 				fieldConfig.resolve = async function (source, args, context, info) {
 					const result = await resolve(source, args, context, info)
@@ -22,3 +18,8 @@ export function upperDirectiveTransformer(schema: GraphQLSchema, directiveName: 
 		},
 	})
 }
+
+const checkDirective = (fieldConfig, directiveName: string): boolean =>
+	fieldConfig.astNode &&
+	fieldConfig.astNode.directives &&
+	fieldConfig.astNode.directives[0].name.value === directiveName
