414a35767cc58502fbbca15b6161fae904ee5354
GreatLaboratory <cartopia95@naver.com>
2022-02-13 20:40:25 +0900

    refactor: extract common interface type model

--- src/domain/authors/models/author.model.ts
+++ src/domain/authors/models/author.model.ts
@@ -1,6 +1,6 @@
 import { Field, ObjectType } from '@nestjs/graphql'
 import { Post } from 'src/domain/posts/models/post.model'
-import { Human } from './human.model'
+import { Human } from '../../../common/models/human.model'
 
 @ObjectType({ implements: () => [Human] })
 export class Author implements Human {
